64c269a0e34ff1253eae8dae722f0fcc5ab388e4
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2021-01-02 21:30:44 -0300

    adicionando navegação na tela Parabens

--- App/telas/Parabens.js
+++ App/telas/Parabens.js
@@ -3,13 +3,14 @@ import { Text, View } from "react-native";
 
 import styles from "../css/telas";
 
-const Parabens = () => {
+const Parabens = ({ navigation }) => {
     return(
         <View style={styles.container}>
             <View style={styles.parabensCircle}></View>
             <Text>{"Parabéns por ter concluido a missão! A galáxia está salva graças a você."}</Text>
             <Text>{"Continue assim."}</Text>
-            <Button title="Proxima Missão" color="#00AA95" accessibilityLabel="Botão para ir para a próxima missão"></Button>
+            <Button title="Proxima Missão" color="#00AA95" accessibilityLabel="Botão para ir para a próxima missão"
+                onPress={() => navigation.navigate('Tarefas do Dia')}></Button>
         </View>
     )
 }
